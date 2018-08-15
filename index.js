954b423d65ab38dbb4f7862f648e9baa2fa26faa
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-08-15 23:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,6 +75,9 @@ function placeOrder(cardNumber){
   if (checkForNumber === false){
     return "Sorry, we don't have a credit card on file for you.";
   } else if (checkForNumber === true){
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    var bill = [];
+    bill.push(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.length = 0;
+    return bill[0];
   }
 }
